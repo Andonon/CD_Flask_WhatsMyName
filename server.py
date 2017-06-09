6a2701c803488a3ac9adac5dd3f74e078d278253
Troy Center <troycenter1@gmail.com>
2017-06-08 18:23:00 -0700

    fixed a couple linter errors, submitted assignment

--- server.py
+++ server.py
@@ -7,15 +7,15 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-
 def landingpage():
+    '''page has form, passes data in.'''
     return render_template("index.html")
 
 @app.route('/process', methods=['GET', 'POST'])
 def processpage():
+    '''This is the process page that the user is redirected to after form submission. '''
     print "Got Post Info"
     print request.form
     name = request.form['name']
     return render_template("process.html", name=name)
 app.run(debug=True)
-
